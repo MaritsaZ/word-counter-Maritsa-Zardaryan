f67be26d3b271ac7a6e884f6791d12a68d1e639f
mmm <origin https://github.com/TempMail1972/temrep.git>
2026-03-05 20:07:45 +0400

    updated files

--- wordcounter.js
+++ wordcounter.js
@@ -1,15 +1,16 @@
 const textarea = document.getElementById('textInput');// Get the textarea element
-        const lettersSpan = document.getElementById('letters');// Get the span element for letters
-        const wordsSpan = document.getElementById('words');// Get the span element for words
-        const sentencesSpan = document.getElementById('sentences');// Get the span element for sentences
-        const button = document.getElementById('countButton');// Get the button element
-
-         // Remove any spaces(\s) and punctuation
-        button.addEventListener('click', () => {
+const lettersSpan = document.getElementById('letters');// Get the span element for letters
+const wordsSpan = document.getElementById('words');// Get the span element for words
+const sentencesSpan = document.getElementById('sentences');// Get the span element for sentences
+const button = document.getElementById('countButton');// Get the button element
+const paragraphsSpan = document.getElementById('paragraphs');// Get the span element for paragraphs
+paragraphsSpan.textContent = 0;// Initialize the paragraphs count to 0         
+// Remove any spaces(\s) and punctuation
+        textarea.addEventListener('input', () => {
         let newText = textarea.value.trim();
         const cleanText = newText.replace(/[\s.,!?]/g, '');// Remove spaces and punctuation from the text, g-flag is used to replace all occurrences
         lettersSpan.textContent = cleanText.length;
-        if (newText === ''){
+        if (newText === ''){// If the trimmed text is empty, set words and sentences count to 0
             wordsSpan.textContent = 0;
             sentencesSpan.textContent = 0;
            
@@ -17,6 +18,7 @@ const textarea = document.getElementById('textInput');// Get the textarea elemen
         else{
             wordsSpan.textContent = newText.split(/\s+/).length;// Split the text by whitespace and count the number of words, if the trimmed text is empty, return 0
             sentencesSpan.textContent = newText.split(/[.!?]+/).filter(s => s.trim() !== '').length;// Split the text by sentence-ending punctuation and count the number of sentences, filtering out any empty sentences
+            paragraphsSpan.textContent = newText.split(/\n{2,}/).filter(p => p.trim() !== '').length;// Split the text by newlines and count the number of paragraphs, filtering out any empty paragraphs
         }
 
     });
